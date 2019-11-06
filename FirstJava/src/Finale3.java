8edce5f31e8055755f7c1c2ac64c780ceb61551a
Nick Chaplin <nikolay.chaplin@aplana.com>
2019-11-06 15:32:35 +0300

    Финальное задание 3

--- FirstJava/src/Finale3.java
+++ FirstJava/src/Finale3.java
@@ -4,5 +4,20 @@
 // (Пример для теста работы программы: Курс доллара = 67,55, Количество рублей: 1000, Итого: 14,80 долларов)
 
 
+import java.util.Scanner;
+
 public class Finale3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вас приветствует конвертер валют КВ-3000");
+        System.out.println("Введите текущий курс доллара:");
+        float x = scanner.nextFloat();
+        System.out.println("Введите сумму в рублях:");
+        int y = scanner.nextInt();
+        float dollar =  y / x;
+        System.out.println("Сумма в долларах:");
+        System.out.printf("%.2f", dollar);
+    }
 }
+
